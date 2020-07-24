33a1c771afa01e6190df10771e1aff32aaf652d1
Adam Hoffman <wintros@yahoo.com>
2020-07-24 16:14:12 -0600

    moved projects up and listed skills in intro

--- client/src/App.js
+++ client/src/App.js
@@ -51,13 +51,85 @@ function App() {
             </div>
             <div className="h1-container">
               <h1 className="h1">Adam Hoffman</h1>
-              <h3 className="full-stack">Full Stack Web Developer</h3>              
+              <h3 className="full-stack">Full Stack Web Developer</h3>
+              <h3 className="full-stack">React || Javascript || Python</h3>
+              <h3 className="full-stack">HTML5 || CSS</h3>
+              <h3 className="full-stack">Express || Node.JS || SQLite</h3>              
+              <h3 className="full-stack">Socket.Io || Heroku || GitHub</h3>                           
             </div>
+        </div>
+        <div className="projects" id="Projects">
+          <h2 className="projects-header">Projects I'm proud of</h2>
+          <div className="project-container">
+            <div className="project1">
+              <img alt="Baseball website screenshot" className="project-image" src={Baseball}></img>
+              <h3>Baseball Game Predictor</h3>
+              <h4>React || Redux || Node.JS || Knex || SQlite</h4>
+              <p>A website built for the purpose of predicting the winner of every baseball game.</p>
+              <div className="more-boxes">
+                <a href="https://github.com/AdamHoffma/baseball-game-prediction-be"><img alt="github icon" className="git-icon" src={Github}></img></a>
+              </div>
+              <div className="more-boxes">
+                <a href="http://www.baseballgamepredictor.com/login"><p>Visit The Site!</p></a>
+              </div>
+              <div className="popup" onClick={popUp}>Learn More...
+                <span className="popuptext" id="myPopup">
+                  <h5>About this project:</h5>
+                  <p>Working with data scientists our groups goal was to create a website that predicted the winners of every MLB game. Our goal was to reach an accuracy above 50%, we achieved 54% by testing our model against previously played games. </p>
+                  <h5>My Role:</h5>
+                  <p>I built the back end routes and database to store data used to create the prediction model. The back end was built using Node.js, SQlite, and Knex. I was also in charge of the personalization of the front end, creating a profile page that was personalized and pleasing. The front end was created using React and Redux. API calls were done via Axios.</p>
+                </span>
+              </div>
+            </div>
+            <div className="project1">
+              <img alt="Design you life website screenshot" className="project-image" src={Design}></img>
+              <h3>Design Your Life</h3>
+              <h4>Node.JS || Knex || SQlite</h4>
+              <p>A beautiful website built to create better habits in you day to day life</p>
+              <div className="more-boxes">
+                <a href="https://github.com/Design-Your-Life-Build-Week/Back-End"><img alt="github icon" className="git-icon" src={Github}></img></a>
+              </div>
+              <div className="more-boxes">
+                <a href="https://designyourlife.now.sh/"><p>Visit The Site!</p></a>
+              </div>
+              <div className="popup2" onClick={popUp2}>Learn More...
+                <span className="popuptext2" id="myPopup2">
+                  <h5>About this project:</h5>
+                  <p>This is a website designed for user to create a one stop page for the habits they would like to create in their life. They can select different categories to place habits under, leave themselves notes on that habit like time of day, reason for the habit or just feedback on how it's going. We also built a 5 star ranking system so the user could rank their enjoyment of that particular habit.</p>
+                  <h5>My Role:</h5>
+                  <p>I was fully in charge of creating the back end for this project. I created the registration and login functionality using Bcrypt and JsonWebTokens. I created the databases for storing a users categories and habits and notes on those habits. I used Knex, Node.JS and Sqlite. I also built all back end routes</p>
+                </span>
+              </div>
+            </div>
+            <div className="project1">
+              <img alt="Chatroom website icon" className="project-image" src={Chat}></img>
+              <h3>Chat App</h3>
+              <h4>Node.JS || React || Socket.Io</h4>
+              <p>Ready to join chatrooms</p>
+              <div className="more-boxes">
+                <a href="https://github.com/AdamHoffma/ReactChatApp"><img alt="github icon" className="git-icon" src={Github}></img></a>
+              </div>
+              <div className="more-boxes">
+                <a href="https://adamchatroom.netlify.app/"><p>Visit The Site!</p></a>
+              </div>
+              <div className="popup3" onClick={popUp3}>Learn More...
+                <span className="popuptext3" id="myPopup3">
+                  <h5>About this project:</h5>
+                  <p>This is a fairly basic chat room app. You enter your name and a room name and it will create a private chatroom. It was built using Node.JS, React and Socket.Io. The primary function of this app was to learn the ins and outs of socket.Io so that I could bring chat features into any app or website I create.</p>
+                  <h5>My Role:</h5>
+                  <p>I built this project on my own, it is all my work. I used a youtube tutorial as a guide and the Socket.Io documentation.</p>
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="About" id="About">
           <div className="about_box">
             <h2>About Me</h2>
-            <p className="about-paragraph">Hi, my name is Adam Hoffman and I am currently a student at Lambda University. I am studying Full Stack Web Development. Prior to being a student at Lambda I ran a successful home remodeling business with my father. I've decided to branch out and learn a new skill set and I've learned so much at Lambda. From the begginnings of HTML and CSS and on into React for front end work and Node.JS for back end work. Check out all the skills I've acquired!</p>
+            <p className="about-paragraph">Hi, my name is Adam Hoffman and I am currently a student at Lambda University. 
+              I am studying Full Stack Web Development. Prior to being a student at Lambda I ran a successful home remodeling business with my father. 
+              I've decided to branch out and learn a new skill set and I've learned so much at Lambda. 
+              From the begginnings of HTML and CSS and on into React for front end work and Node.JS for back end work. Check out all the skills I've acquired!</p>
           </div>
           <div className="skills">
             <h2>Skills and Technologies</h2>
